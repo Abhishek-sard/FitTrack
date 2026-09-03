e246eadda1277e0ce0813b27a8b81de669415dac
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-09-03 16:39:29 +0545

    updated

---
+++ client/src/components/Pages/Workouts/AllExercises.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllExercises = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllExercises
---
+++ client/src/components/Pages/Workouts/MyWorkouts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyWorkouts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyWorkouts
