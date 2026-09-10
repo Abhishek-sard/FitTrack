6ff5db554bb5a6e4ba30395b1af341c9549ad0ba
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-09-10 11:24:37 +0545

    adminmiddleware.js

---
+++ Backend/middleware/adminMiddleware.js
@@ -0,0 +1,17 @@
+const admin = (req, res, next) => {
+    if(!req.user){
+        return res.status(401).json({
+            success: false,
+            message: "Not authenticated.",
+        });
+    }
+    if(req.user.role !== "admin"){
+        return res.status(403).json({
+            success: false,
+            message: "You are not authorized to access this resource.",
+        });
+    }
+    next();
+};
+
+export { admin };
